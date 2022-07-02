2176123a7279a17c9457a34b12d53761484447a5
George Krotov <zvuk24@gmail.com>
2022-07-02 17:21:57 +0700

    Добавьте файлы проекта.

---
+++ File_manager.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <filesystem>
+#include <Windows.h>
+#include <vector>
+using namespace std;
+namespace fs = std::filesystem;
+
+int main(int argс, char* argv[]) // включим аргументы командной строки для принятия пути каталога, из которого начнет работать программа
+{
+	fs::path p;
+	if (argс > 1) // если передан аргумент - присваиваем его родительскому пути
+	{
+		p = fs::path(*argv);
+	}
+	
+	cout << "n" << p.root_directory();
+}
