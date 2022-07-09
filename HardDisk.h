930e4c68a4c0327ce0f5db6464e0672f106c93e7
Кротов Георгий <zvuk24@gmail.com>
2022-07-09 15:44:22 +0700

    2

--- HardDisk.h
+++ HardDisk.h
@@ -19,7 +19,6 @@ public:
 
 	HardDisk() // в конструкторе по умолчанию заполняется встроенный список структурами с информацией о дисках
 	{
-		
 		char m[1024];
 		char sm[1024];
 		char t[1024];
@@ -31,39 +30,34 @@ public:
 				char sm[1024];
 				DWORD ln;
 				GetVolumePathNamesForVolumeNameA(m, sm, 1024, &ln);
-
 				Hard hr;
-
 				hr.name = sm;
 				std::error_code er;
 				if ((hr.name != "") && fs::exists(hr.name, er))
 				{
 					switch (GetDriveTypeA(m))
 					{
-					case DRIVE_REMOVABLE:
-					{
-						hr.type = "REMOVABLE";
-						break;
-					}
-					case DRIVE_FIXED:
-					{
-						hr.type = "FIXED";
-						break;
-					}
-					case DRIVE_REMOTE:
-					{
-						hr.type = "REMOTE";
-						break;
-					}
-
-
+						case DRIVE_REMOVABLE:
+						{
+							hr.type = "REMOVABLE";
+							break;
+						}
+						case DRIVE_FIXED:
+						{
+							hr.type = "FIXED";
+							break;
+						}
+						case DRIVE_REMOTE:
+						{
+							hr.type = "REMOTE";
+							break;
+						}
 					}
 				}
 				lst.push_back(hr);
 			} while (FindNextVolumeA(find, m, 1024));
 			FindVolumeClose(find);
 		}
-		
 	}
 };
 
